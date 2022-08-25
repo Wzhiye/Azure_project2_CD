464fe057ca2e6ffaab12cab6e07f4685791f69a5
Zhiye Wen <66264323+Wzhiye@users.noreply.github.com>
2022-08-25 22:39:10 +0200

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -46,5 +46,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://flaskappwen.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
